2db7ec2a954270a92144b4457c7ef4460e43ac28
Горячев Владимир Сергеевич <51385107+Nomalno@users.noreply.github.com>
2022-10-10 22:46:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+{
+    Console.WriteLine("Число десятков первого числа: ");
+    int a2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число единц первого числа: ");
+    int a1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число десятков второго числа: ");
+    int b2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число единц второго числа: ");
+    int b1 = Convert.ToInt32(Console.ReadLine());
+    a2 = a2 + b2 + (a1 + b1) / 10;
+    a1 = (a1 + b1) % 10;
+    Console.WriteLine($"c={a2}{a1}");
+}
+
+//Даны цифры двух двузначных чисел, записываемых в виде a2a1 и b2b1, где 1a и 1b—число единиц, 2a и 2b—число десятков.
+//Получить цифры числа, равного сумме заданных чисел (известно, что это число двузначное).
+//Слагаемое-двузначное число и число-результат не определять; условный оператор не использовать.
